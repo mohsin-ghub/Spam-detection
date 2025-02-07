b83826c4560e206e23450545ab20b52e810498b0
unknown <mohhammed895@gmail.com>
2025-02-07 20:25:26 +0530

    Added spam detection script and dataset

--- spam.py
+++ spam.py
@@ -35,7 +35,7 @@ from sklearn.metrics import accuracy_score, classification_report
 X = vectorizer.fit_transform(df['message'])  # Already transformed
 y = df['label']  # Convert labels to binary (0 = ham, 1 = spam)
 
-df = df.dropna(subset=['message'])
+
 #print(df.columns)
 # Split data into training and testing sets (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
